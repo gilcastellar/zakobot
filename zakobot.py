bf58c1274dbff9ea6903e3c29486532d5767e462
Kaiser <Kaiser@DESKTOP-MOCQ3QQ>
2023-01-13 04:18:59 -0300

    Add project files.

---
+++ zakobot.py
@@ -0,0 +1,20 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have loggeg in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith(';;hello'):
+        await message.channel.send('Hello!')
+
+client.run('MTA2MzM1MTg3MjAwMzUxODQ5NQ.Ga729N.ZK5x4_eIsS3pQNT6iZ_I3VIPdRhCEdSu5-wGUE')
