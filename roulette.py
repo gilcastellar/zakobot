7cd96a8570056f34abc92ec866f80968e9da6ffd
gilcastellar <gilcastellar@gmail.com>
2023-01-14 01:56:04 -0300

    - added roulette commands

---
+++ roulette.py
@@ -0,0 +1,8 @@
+def create_roulette():
+    roulette_list = []
+
+def add_roulette_member(member):
+    roulette_list.append(member)
+
+def roulette_members():
+    return roulette_list
--- zakobot.py
+++ zakobot.py
@@ -1,20 +1,63 @@
 import discord
+import random
+import asyncio
+
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user} (ID: {self.user.id})')
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith(';;hi'):
+            await message.channel.send('Hi!' + message.content)
+
+        if message.content.startswith(';;anilist'):
+            command, content = message.content.split(" ")
+            await message.channel.send(content)
+
+        if message.content.startswith(';;newr'):
+            global roulette
+            roulette = []
+            await message.channel.send('Roulette created!')
+            #roulette.create_roulette()
+
+        if message.content.startswith(';;radd'):
+            command, content = message.content.split(" ")
+            roulette.append(content)
+            await message.channel.send('New roulette member added!')
+            #roulette.add_roulette_member(content)
+
+        if message.content.startswith(';;rmembers'):
+            for member in roulette:
+                await message.channel.send(member)
+
+        if message.content.startswith(';;guess'):
+            await message.channel.send('Guess a number between 1 and 10.')
+
+            def is_correct(m):
+                return m.author == message.author and m.content.isdigit()
+
+            answer = random.randint(1, 10)
+
+            try:
+                guess = await self.wait_for('message', check=is_correct, timeout=5.0)
+            except asyncio.TimeoutError:
+                return await message.channel.send(f'Sorry, you took too long it was {answer}.')
+
+            if int(guess.content) == answer:
+                await message.channel.send('You are right!')
+            else:
+                await message.channel.send(f'Oops. It is actually {answer}.')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'We have loggeg in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith(';;hi'):
-        await message.channel.send('Hi!')
+client = MyClient(intents=intents)
 
 client.run('MTA2MzM1MTg3MjAwMzUxODQ5NQ.Ga729N.ZK5x4_eIsS3pQNT6iZ_I3VIPdRhCEdSu5-wGUE')
