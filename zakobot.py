dd9c6a886367d5085e6cbe9f85339f7d3ba8e61b
gilcastellar <gilcastellar@gmail.com>
2023-01-15 23:45:18 -0300

    - código limpo

--- zakobot.py
+++ zakobot.py
@@ -9,16 +9,11 @@ class MyClient(discord.Client):
         print('------')
 
     async def on_message(self, message):
-        # we do not want the bot to reply to itself
         if message.author.id == self.user.id:
             return
 
-        #if message.content.startswith(';salve'):
-        #    await message.channel.send('SALVE O CORINTHIANS!!')
-
         if message.content.startswith(';hi'):
             await message.channel.send('Hi ' + str(message.author))
-
 
         if message.content.startswith(';anilist'):
             command, content = message.content.split(" ")
@@ -38,42 +33,26 @@ class MyClient(discord.Client):
 
             roulette = roulettetools.add_roulette_member(roulette,content)
             await message.channel.send('New roulette member added!')
-            #roulette.add_roulette_member(content)
 
         if message.content.startswith(';rmembers'):
-            #global members
             members = ''
             for member in roulette:
                 members += member + "\n"
             await message.channel.send(members)
 
         if message.content.startswith(';shuffler'):
-            #await message.channel.send(roulette)
 
             shuffled = roulettetools.shuffle_roulette(roulette)
-
-            #await message.channel.send(shuffled)
-
+            
             final_roulette = roulettetools.format(shuffled)
 
             embed = discord.Embed(title='Roleta formada!', description=final_roulette)
             await message.channel.send(embed=embed)
 
-            #await message.channel.send('**Roulette shuffled!** \n' + final_roulette)
-
-        #if message.content.startswith(';embed'):
-        #    embed = discord.Embed(title='Roleta formada!', description=)
-        #    await message.channel.send(embed=embed)
-
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-
-#@client.command()
-#async def embed(ctx):
-#    embed = discord.Embed(title='Example Dmbed', description='this is an embed')
-#    await ctx.send(embed=embed)
 
 
 client.run('MTA2MzM1MTg3MjAwMzUxODQ5NQ.Ga729N.ZK5x4_eIsS3pQNT6iZ_I3VIPdRhCEdSu5-wGUE')
