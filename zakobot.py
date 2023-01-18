0da5cc80bfa8c59d90741457c179e4db85a3359c
gilcastellar <gilcastellar@gmail.com>
2023-01-18 03:29:29 -0300

    remoção da classe MyClient

--- zakobot.py
+++ zakobot.py
@@ -3,70 +3,77 @@ import random
 import roulettetools
 import configparser
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-
-        if message.content.startswith(';hi'):
-            await message.channel.send('Hi ' + str(message.author))
-
-        if message.content.startswith(';anilist'):
-            command, content = message.content.split(" ")
-            await message.channel.send(content)
-
-        if message.content.startswith(';shini'):
-            adjetivos = ['gay','viadao','homossexual','boiola','bicha','senta-fofo','morde-fronhas','maricas','pederasta','baitola']
-            await message.channel.send('O Shini eh muito ' + random.choice(adjetivos))
-
-        if message.content.startswith(';radd'):
-            command, content = message.content.split(" ")
-
-            if 'roulette' not in globals():
-                global roulette 
-                roulette = roulettetools.create_roulette()
-                await message.channel.send('Roulette created!')
-
-            roulette = roulettetools.add_roulette_member(roulette,content)
-            await message.channel.send('New roulette member added!')
-
-        if message.content.startswith(';rremove'):
-            command, content = message.content.split(" ")
-
-            roulette = roulettetools.remove_roulette_member(roulette,content)
-            await message.channel.send('Roulette member removed!')
-
-        if message.content.startswith(';rmembers'):
-            members = ''
-            for member in roulette:
-                members += member + "\n"
-            await message.channel.send(members)
-
-        if message.content.startswith(';shuffler'):
-
-            global previous_roulette
-            previous_roulette = open('original_roulette.txt')
-            print('original: ' + previous_roulette.read().replace(',',' ').title())
-
-            shuffled, previous_roulette = roulettetools.shuffle_roulette(roulette, previous_roulette)
-
-            print(previous_roulette)
-            with open('previous_roulette.txt', 'w') as file:
-                file.write(previous_roulette)
-            
-            final_roulette = roulettetools.format(shuffled)
-
-            embed = discord.Embed(title='Roleta formada!', description=final_roulette)
-            await message.channel.send(embed=embed)
-
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = MyClient(intents=intents)
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author.id == client.user:
+        return
+
+    if message.content.startswith(';hi'):
+        await message.channel.send('Hi ' + str(message.author))
+
+    if message.content.startswith(';anilist'):
+        command, content = message.content.split(" ")
+        await message.channel.send(content)
+
+    if message.content.startswith(';shini'):
+        adjetivos = ['gay','viadao','homossexual','boiola','bicha','senta-fofo','morde-fronhas','maricas','pederasta','baitola']
+        await message.channel.send('O Shini eh muito ' + random.choice(adjetivos))
+
+    if message.content.startswith(';radd'):
+        command, content = message.content.split(" ")
+
+        if 'roulette' not in globals():
+            global roulette 
+            roulette = roulettetools.create_roulette()
+            await message.channel.send('Roulette created!')
+
+        roulette = roulettetools.add_roulette_member(roulette,content)
+        await message.channel.send('New roulette member added!')
+
+    if message.content.startswith(';rremove'):
+        command, content = message.content.split(" ")
+
+        roulette = roulettetools.remove_roulette_member(roulette,content)
+        await message.channel.send('Roulette member removed!')
+
+    if message.content.startswith(';rmembers'):
+        members = ''
+        for member in roulette:
+            members += member + "\n"
+        await message.channel.send(members)
+
+    if message.content.startswith(';test'):
+        #command, content = message.content.split(" ")
+
+        #print(content.strip('<>@'))
+        await message.channel.send('I found you, <@249670889438838794>')
+
+    if message.content.startswith(';shuffler'):
+
+        global previous_roulette
+        previous_roulette = open('original_roulette.txt')
+        #print('original: ' + previous_roulette.read().replace(',',' ').title())
+
+        shuffled, previous_roulette = roulettetools.shuffle_roulette(roulette, previous_roulette)
+
+        print(previous_roulette)
+        with open('previous_roulette.txt', 'w') as file:
+            file.write(previous_roulette)
+            
+        final_roulette = roulettetools.format(shuffled)
+
+        embed = discord.Embed(title='Roleta formada!', description=final_roulette)
+        await message.channel.send(embed=embed)
+
 
 config = configparser.RawConfigParser()
 config.read('app.properties')
