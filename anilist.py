91a59a8c5d4232779001bcf05b142f0407677b04
gilcastellar <gilcastellar@gmail.com>
2023-01-29 22:11:34 -0300

    teste de mutation

--- anilist.py
+++ anilist.py
@@ -27,3 +27,28 @@ def test_anilist(url):
 
     # Make the HTTP Api request
     return requests.post(url, json={'query': query, 'variables': variables})
+
+def test_mutation(anime, token):
+    headers = {
+        'Authorization': token
+        }
+
+    mutation = '''
+    mutation ($mediaId: Int, $status: MediaListStatus) {
+    SaveMediaListEntry (mediaId: $mediaId, status: $status) {
+        id
+        status
+        }
+    }
+    '''
+
+    # Define our query variables and values that will be used in the query request
+    variables = {
+        "mediaId" => 1,
+        "status" => "CURRENT"
+    }
+
+    data = (requests.post('https://graphql.anilist.co', json={'query': mutation, headers=headers}))
+
+    print(data)
+       
