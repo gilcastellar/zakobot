b9c1b907888e78428447109937d186992d8d14e6
gilcastellar <gilcastellar@gmail.com>
2023-02-01 00:47:57 -0300

    teste

--- idleanime.py
+++ idleanime.py
@@ -16,6 +16,6 @@ options = ['335','481','383','634','513']
 # delay: 30 minutos, a menos que esteja assistindo algo
 # neste caso empurra a decisão para 5 minutos após finalizar
 
-@tasks.loop(seconds = 1) # repeat after every 10 seconds
+@tasks.loop(seconds = 1, count = len(options)) # repeat after every 10 seconds
 async def start_anime():
     print('boa')
