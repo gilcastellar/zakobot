90e1dd6ec3deaa5e1f87d19cd6b5772f9336bc07
gilcastellar <gilcastellar@gmail.com>
2023-05-19 12:10:04 -0300

    t

--- dbservice.py
+++ dbservice.py
@@ -1,3 +1,5 @@
+import database
+
 def update(table, columns, values, where_col, where_val):
 
     sql = 'UPDATE ' + table + ' SET '
@@ -5,12 +7,43 @@ def update(table, columns, values, where_col, where_val):
     idx = 0
 
     for column in columns:
+
         if idx != 0:
+
             sql += ', '
+
         sql += column + '="' + values[idx] + '"'
         idx += 1
 
     sql += ' WHERE ' + where_col + '="' + where_val + '"'
 
     print(sql)
-    ...
+    
+    database.update(sql)
+
+def insert(table, columns, values):
+
+    sql = 'INSERT INTO ' + table + ' ('
+
+    val = '('
+
+    _val = '(%s'
+
+    for column in columns:
+
+        sql += column + ','
+
+        _val += ',%s'
+
+    for value in values:
+
+        val += value + ','
+
+    _val = _val.strip(',') + ')'
+    
+    val = val.strip(',') + ')'
+
+    sql += ') VALUES (' + _val
+
+    print(sql)
+    print(val)
