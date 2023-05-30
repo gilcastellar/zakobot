348d126724793ee5b2a06e999d86804daf7d0ded
gilcastellar <gilcastellar@gmail.com>
2023-05-30 12:46:17 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -2175,8 +2175,11 @@ async def ofertar_command(
     max_own = dbservice.select('user_has_chara', ['quantity'], '', {'chara_name': own_chara})
     max_target = dbservice.select('user_has_chara', ['quantity'], '', {'chara_name': target_chara})
 
-    max_own = from_list_of_tuples_to_list(max_own)
-    max_target = from_list_of_tuples_to_list(max_target)
+    if type(max_own) != int:
+        max_own = from_list_of_tuples_to_list(max_own)
+
+    if type(max_target) != int:
+        max_target = from_list_of_tuples_to_list(max_target)
 
     if max_own == None:
         max_own = 1
