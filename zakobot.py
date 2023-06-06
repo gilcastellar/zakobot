40f8fcb018d453786ee87c9a4deda3f78a788379
gilcastellar <gilcastellar@gmail.com>
2023-06-06 11:21:23 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -2230,7 +2230,7 @@ async def generate_collection(msg, user_id, page, last_page):
 
     indice = (page * 25) - 24
 
-    text = '```'
+    text = '``` Personagem:                                            Cópias:                           Posição:'
 
     print('page')
     print(page)
