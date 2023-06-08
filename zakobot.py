e6992bfd86b43e35e06f76c81f9660e3c37ee18a
gilcastellar <gilcastellar@gmail.com>
2023-06-08 12:29:42 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -2223,7 +2223,7 @@ async def generate_collection(msg, user_id, page, last_page):
           
         chara_text = f'{str(chara_info[0])} ({chara_info[1]})'
 
-        while len(chara_text) < 100: 
+        while len(chara_text) < 95: 
             chara_text += ' '
 
         copies_text = (' ' * (3 - len(str(copies)))) + str(copies)
