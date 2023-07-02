c07de8cbe70db74814c28f45f5029c8d68776dbd
gilcastellar <gilcastellar@gmail.com>
2023-07-01 21:54:05 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -1280,8 +1280,11 @@ async def get_user_avg(user):
     #scores_given = database.selectall(sql, True)
 
     scores_given = dbservice.select('user_has_roleta', ['score'], 'ORDER BY id_roleta', {'id_receiver': str(user.id)})
+    print(scores_given)
 
     scores_given = from_list_of_tuples_to_list(scores_given)
+
+    print(scores_given)
 
     print('I was tasked with getting ' + str(user.display_name) + '\'s given scores. They are below:')
     print(scores_given)
