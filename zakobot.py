184e9b4f8cacef34f03f19cc20eef7d2a444140b
gilcastellar <gilcastellar@gmail.com>
2024-01-03 22:30:55 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -3503,6 +3503,22 @@ async def gerar_classificados(msg, page, last_page):
 
 # melhoras o comando de terminar
 
+@mercado.command(name='inventario')
+async def inventario_command(
+    ctx: discord.ApplicationContext
+):
+    user_id = ctx.author.id    
+
+    data = dbservice.select('mercado', ['item_url', 'item_name', 'item_type', 'value', 'date_inserted'], '', {'is_available':'true', 'buyer': user_id})
+
+    print(data)
+    
+    await ctx.respond(f'MEU INVENTÁRIO')
+    
+    msg = await create_placeholder_message(ctx, ctx.interaction.channel.id)
+
+    await gerar_classificados(msg, 1, 0)
+
 # Auxiliar command
 @bot.command(name='aux')
 async def aux_command(ctx):
