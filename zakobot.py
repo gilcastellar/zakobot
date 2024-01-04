504963952933c9ee1870893a747a9734994b9460
gilcastellar <gilcastellar@gmail.com>
2024-01-04 11:25:49 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -921,7 +921,7 @@ async def create_placeholder_message(ctx, channel_id, ephemeral=False):
     if ephemeral == False:
         return await send_message(ctx, 'Carregando...', channel_id)
     else:
-        return await send_message(ctx, 'Carregando...', channel_id,ephemeral=True)
+        return await send_message(ctx, 'Carregando...', channel_id, ephemeral=True)
         
 
 # Generate board
@@ -3491,7 +3491,7 @@ async def inventario_command(
         await ctx.response.send_message(text + 'Você não tem nada em seu inventário.', ephemeral=True)
         return
     
-    msg = await create_placeholder_message(ctx, ctx.interaction.channel.id)
+    msg = await create_placeholder_message(ctx, ctx.interaction.channel.id, True)
 
     # await gerar_inventario(msg, 1, 0, user_id)
     
