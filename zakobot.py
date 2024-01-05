a6d291bf9824b04bd7223d9a615ad7248b8bf772
gilcastellar <gilcastellar@gmail.com>
2024-01-05 15:59:57 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -3461,7 +3461,7 @@ async def guilda_abandonar_quest_command(
             flavor1, flavor2 = dbservice.select('quests', ['flavor_text'], '', {'buyer': user_id, 'id_anilist': anilist_id}).split('*')
             dbservice.update('quests', ['buyer', 'is_available', 'abandoned'], ['', 'true', 'true'], {'buyer': user_id, 'id_anilist': anilist_id})
             
-            await ctx.response.send_message('<@' + str(user_id) + '> desistiu da quest ' + flavor1 + '**' + obra + '**' + flavor2 + ' e ela foi devolvida ao quadro. Essa quest não conta para o limite de criação do criador.')
+            await ctx.response.send_message('<@' + str(user_id) + '> desistiu da quest "' + flavor1 + '**' + obra + '**' + flavor2 + '" e a mesma foi devolvida ao quadro. Essa quest não conta para o limite de criação do criador.')
             
         else:
 
