3b95117b81a00a19c826290299748921efda1e07
gilcastellar <gilcastellar@gmail.com>
2024-01-07 14:08:16 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -4116,7 +4116,7 @@ async def formar_grupo_command(
     print(quest)
     print(grupo)
     
-    # dbservice.update('quests', ['buyer', 'is_available', 'date_bought'], [grupo, 'false', int(datetime.datetime.now().timestamp())])
+    dbservice.update('quests', ['buyer', 'is_available', 'date_bought'], [grupo, 'false', int(datetime.datetime.now().timestamp())])
     
     await ctx.response.send_message('Grupo criado com sucesso!', ephemeral=True)
     
