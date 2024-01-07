b60c10388ec9f29db33a78ddc845630f6aeb17ab
gilcastellar <gilcastellar@gmail.com>
2024-01-07 13:20:54 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -4049,11 +4049,10 @@ async def formar_grupo_command(
     # dbservice.update('quests', ['buyer', 'is_available', 'date_bought'], [grupo, 'false', int(datetime.datetime.now().timestamp())])
     
     await ctx.response.send_message('Grupo criado com sucesso!', ephemeral=True)
-    
-    print("TESTETETE:")
-    print(dbservice.select('quests', ['flavor_text'], '', {'item_name': quest}))
 
-    flavor1, flavor2 = dbservice.select('quests', ['flavor_text'], '', {'item_name': quest}).split('*')
+    flavor = random.choice(dbservice.select('quest_flavors', ['flavor'], ''))[0]
+
+    flavor1, flavor2 = flavor.split('*')
 
     leader = dbservice.select('user', ['name'], '', {'id': ctx.author.id})    
 
