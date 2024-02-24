45f0b0fe7ab8ffb57051462cfa49b865a55b7bcf
gilcastellar <gilcastellar@gmail.com>
2024-02-24 11:17:55 -0300

    t

--- zakobot.py
+++ zakobot.py
@@ -4171,9 +4171,9 @@ async def generate_banner():
         img = chara[3]
         value = chara[4]
         print(name)
-        await send_message2(f'Com o valor total de {str(value)} zakoletas...', channel)
+        await send_message2(f'\n\nCom o valor total de {str(value)} zakoletas...', channel)
         await asyncio.sleep(3)
-        await send_message2(f' ­ \n\n\n\n\n[{name}]({img})!­­', channel)
+        await send_message2(f'[{name}]({img})!­­^\n{url}', channel)
         await asyncio.sleep(5)
         await asyncio.sleep(5)
         dbservice.insert('gacha_chara', ['id', 'url', 'name', 'img', 'status'], [id, url, name, img, 'new'])
